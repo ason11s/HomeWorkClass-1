7234b862611377b5595da55d60a1d5e626d1fcd1
asonovserg <ason11s@bk.ru>
2025-03-24 16:59:04 +0500

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+
+        Author author1 = new Author("Джоан" , "Роллинг");
+        Author author2 = new Author("Джон" , "Толкин");
+
+        Book book1 = new Book("Гарри Потер и филосовский камень", author1, 1995);
+        Book book2 = new Book("Властелин колец", author2, 1937);
+
+        System.out.println(book1);
+        System.out.println(book2);
+
+
+        book1.setPublicationYear(2000);
+
+        System.out.println(" Информация о книгах после изменения : ");
+        System.out.println(book1);
+        System.out.println(book2);
+
+
     }
 }
